b78c38bc6d1aee5c01721fd704c106f6b84dc803
Christopher Barnes <senrabc@gmail.com>
2015-08-13 12:05:08 -0400

    changed output file name.

--- provision/vivo/contrib/export-all-triples.sh
+++ provision/vivo/contrib/export-all-triples.sh
@@ -3,4 +3,4 @@
 # using the custom mysql query in mysql-query-all-triples.sql this script will
 # export all triples out in an N-triples format. This format was extrapolated from 
 # matching the output of the "rdf export" in the site admin of the empty vagrant
-mysql -uroot -pvivo vivodev < mysql-query-all-triples.sql > out5.txt
+mysql -uroot -pvivo vivodev < mysql-query-all-triples.sql > out.txt
